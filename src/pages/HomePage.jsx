f4c867e9b0ea3be0fe2fefd8cf2b61c74e142793
rayfeng <ryfprivate@outlook.com>
2019-04-21 20:36:27 +1000

    Added HomePage with fixed background

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-import text from './text';
+import image from '../images/space.jpg';
 
-const Wrap = styled.div`
-  background: var(--primary-dark);
+const Page = styled.div`
   color: var(--primary-light);
+  display: flex;
+  flex-direction: column;
+  margin-top: 54px;
+  height: 91vh;
+  background: url(${image});
+  background-size: cover;
+  background-position: 20% 50%;
+  background-attachment: fixed;
+`;
+
+const Content = styled.div`
+  margin: auto 0;
+  text-align: center;
+`;
+
+const Header = styled.h2`
+  font-size: 40px;
+  margin-bottom: 0;
+`;
+
+const Subheader = styled.p`
+  font-size: 20px;
+`;
+
+const Span = styled.span`
+  color: #fee9c4;
 `;
 
 export default () => {
   return (
-    <Wrap>
-      <h2>HomePage</h2>
-      <p>{text}</p>
-    </Wrap>
+    <Page>
+      <Content>
+        <Header>
+          Hi, my name is <Span>Ray</Span>
+        </Header>
+        <Subheader>Welcome to my website</Subheader>
+      </Content>
+    </Page>
   );
 };
