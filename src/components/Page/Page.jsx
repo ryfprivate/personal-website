d433ff3d86f1e9c0dce2a4eb8686a3ac0ffe5b9d
rayfeng <ryfprivate@outlook.com>
2019-04-25 18:47:44 +1000

    Edited Page component for background styling

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -3,23 +3,15 @@ import styled from 'styled-components';
 import { Element } from 'react-scroll';
 
 const Wrap = styled.div`
-  height: 91vh;
-  background-color: ${props =>
-    props.dark ? 'var(--primary-dark)' : 'var(--primary-light)'};
-  color: ${props =>
-    props.dark ? 'var(--primary-light)' : 'var(--primary-dark)'};};
+  height: 100vh;
+  padding: 1em 4.5em;
+  ${props => props.background}
 `;
 
-const Header = styled.h2`
-  margin: 0;
-`;
-export default ({ name, header, children, dark }) => {
+export default ({ name, background, children }) => {
   return (
     <Element name={name}>
-      <Wrap dark={dark}>
-        <Header>{header}</Header>
-        {children}
-      </Wrap>
+      <Wrap background={background}>{children}</Wrap>
     </Element>
   );
 };
