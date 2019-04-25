a72d9c536b1682010586e97adfd2450f5f452ae5
rayfeng <ryfprivate@outlook.com>
2019-04-25 18:48:19 +1000

    Added HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import Page from '../components/Page';
 import image from '../images/space.jpg';
 
-const Page = styled.div`
-  color: var(--primary-light);
-  display: flex;
-  flex-direction: column;
-  margin-top: 54px;
-  height: 91vh;
-  background: url(${image});
+const gradient = 'rgba(0, 0, 0, 0.0)';
+const background = `
+  background: linear-gradient(${gradient}, ${gradient}), url(${image});
   background-size: cover;
   background-position: 20% 50%;
   background-attachment: fixed;
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 `;
 
 const Content = styled.div`
@@ -19,28 +22,52 @@ const Content = styled.div`
   text-align: center;
 `;
 
+const TextContainer = styled.div`
+  margin: 0 auto;
+  background: var(--primary-light);
+  opacity: 0.5;
+`;
+
 const Header = styled.h2`
+  ${background};
   font-size: 40px;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
   margin-bottom: 0;
 `;
 
 const Subheader = styled.p`
+  ${background};
   font-size: 20px;
+  font-weight: 600;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  margin-top: 0;
+  padding-bottom: 1%;
 `;
 
-const Span = styled.span`
-  color: #fee9c4;
-`;
+class HomePage extends React.Component {
+  componentDidMount() {
+    console.log('mounted home');
+  }
+  render() {
+    return (
+      <Page name="home" background={background}>
+        <Wrap
+          onScroll={() => {
+            console.log('function');
+          }}
+        >
+          <Content>
+            <TextContainer>
+              <Header>Hi, my name is Ray</Header>
+              <Subheader>Welcome to my website</Subheader>
+            </TextContainer>
+          </Content>
+        </Wrap>
+      </Page>
+    );
+  }
+}
 
-export default () => {
-  return (
-    <Page>
-      <Content>
-        <Header>
-          Hi, my name is <Span>Ray</Span>
-        </Header>
-        <Subheader>Welcome to my website</Subheader>
-      </Content>
-    </Page>
-  );
-};
+export default HomePage;
