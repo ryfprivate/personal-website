cca5af9858106071a5dd14d520f70b353392a511
rayfeng <ryfprivate@outlook.com>
2019-04-30 20:21:35 +1000

    Added all pages and page end div

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import SkillsPage from './pages/SkillsPage';
+import ExperiencePage from './pages/ExperiencePage';
 
 const Layout = styled.div`
   --primary-dark: #282a36;
+  --secondary-dark: #6272a4;
   --primary-light: #f8f8f2;
+  --secondary-light: #8be9fd;
   display: flex;
   flex-direction: column;
 `;
@@ -19,6 +22,10 @@ class App extends React.Component {
         <HomePage />
         <ProfilePage />
         <SkillsPage />
+        <ExperiencePage />
+        <div>
+          <h2>Page end</h2>
+        </div>
       </Layout>
     );
   }
